5e8f2fbcfd5249e9023e6782e41a4c311a18ba5b
Spencer Heywood <github@heywoodlh.io>
2024-06-06 16:10:37 -0600

    updates to packages and wsl

--- config.ps1
+++ config.ps1
@@ -22,6 +22,7 @@ New-Item -ErrorAction silentlycontinue -Type Directory ~/.ssh
 Copy-Item dotfiles/ssh/config ~/.ssh/config
 
 
+
 # Configure PowerShell
 New-Item -ErrorAction silentlycontinue -Type Directory $(Split-Path $PROFILE)
 Copy-Item dotfiles/profile.ps1 $PROFILE
@@ -34,11 +35,11 @@ Copy-Item dotfiles/powershell/docker.ps1 ~/.config/powershell/docker.ps1
 # Disable ctrl+super+s key (requires reboot)
 # Doesn't seem to work, remap ctrl+super+s
 # Use PowerToys Keyboard, use another bind
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name DisabledHotkeys -Value "SA" -Force
+#Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name DisabledHotkeys -Value "SA" -Force
 
 
 
-# Configure Vim
+# Configure Vim for Windows
 Copy-Item dotfiles/vimrc ~/_vimrc
 New-Item -ErrorAction silentlycontinue -Type Directory ~/temp
 Invoke-WebRequest -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
@@ -52,21 +53,21 @@ Copy-Item dotfiles/gitconfig ~/.gitconfig
 
 
 # Configure Windows Terminal
-#New-Item -Type Directory ~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/ -ErrorAction silentlycontinue
-#Copy-Item dotfiles/windows-terminal.json ~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
+New-Item -Type Directory ~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/ -ErrorAction silentlycontinue
+Copy-Item dotfiles/windows-terminal.json ~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
 #Copy-Item resources/wt-startup.bat $startupPath/wt-startup.bat
 #Get-Process -Name "WindowsTerminal" 2> $null || Start-Process wt.exe -ArgumentList '-w _quake' -WindowStyle hidden
 
 
 
 # Configure Komorebi
-Copy-Item dotfiles/komorebi-generated.ps1 ~/komorebi-generated.ps1
-Copy-Item dotfiles/komorebi.ps1 ~/komorebi.ps1
-Copy-Item dotfiles/whkdrc ~/.config/whkdrc
+#Copy-Item dotfiles/komorebi-generated.ps1 ~/komorebi-generated.ps1
+#Copy-Item dotfiles/komorebi.ps1 ~/komorebi.ps1
+#Copy-Item dotfiles/whkdrc ~/.config/whkdrc
 # Start Komorebi on login
-. ./scripts/komorebi-startup.ps1
+#. ./scripts/komorebi-startup.ps1
 # Start Komorebi
-Get-Process -Name "komorebi" 2> $null || Start-Process komorebi.exe -ArgumentList '--await-configuration' -WindowStyle hidden
+#Get-Process -Name "komorebi" 2> $null || Start-Process komorebi.exe -ArgumentList '--await-configuration' -WindowStyle hidden
 
 
 
@@ -99,16 +100,22 @@ Get-Process -Name "hide-mouse-cursor" 2> $null || & "$startupPath/hide-mouse-cur
 
 
 
-# Install and configure Wezterm
-if ((test-path $HOME\bin\wezterm.exe) -eq $False) {
-    Invoke-WebRequest -Uri https://github.com/wez/wezterm/releases/download/nightly/WezTerm-windows-nightly.zip -OutFile ~/Downloads/wezterm.zip
-    Expand-Archive ~/Downloads/wezterm.zip -DestinationPath ~/Downloads/wezterm
-    Copy-Item ~/Downloads/wezterm/*/* ~/bin/
-}
-
-Copy-Item ./dotfiles/wezterm.lua ~/.wezterm.lua
-
-
-
 # Remove all pinned items
 Copy-Item -Force resources/start2.bin $env:LOCALAPPDATA\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState\start2.bin
+
+
+
+# Download NixOS-WSL from latest release
+curl.exe --silent -L -C - https://github.com/nix-community/NixOS-WSL/releases/latest/download/nixos-wsl.tar.gz -o $HOME\Documents\nixos-wsl.tar.gz
+if (C:\Windows\system32\wsl.exe --list | select-string nixos)
+{
+  write-output "wsl already set up with nixos"
+} else {
+  write-output "installing nixos-wsl"
+  C:\Windows\system32\wsl.exe --update
+  new-item -itemtype directory $HOME\Documents\wsl
+  C:\Windows\system32\wsl.exe --import nixos $env:USERPROFILE\Documents\wsl\nixos\ $HOME\Documents\wsl\nixos-wsl.tar.gz
+  C:\Windows\system32\wsl.exe --set-version nixos 2
+  C:\Windows\system32\wsl.exe --set-default nixos
+  C:\Windows\system32\wsl.exe -d nixos
+}
